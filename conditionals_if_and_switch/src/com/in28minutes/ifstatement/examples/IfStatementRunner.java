881cd5df31432c4c014124f6f959b55a17915f8b
user <user@20.20.4.163>
2024-01-18 12:02:22 +0900

    if else 구문

--- conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/IfStatementRunner.java
+++ conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/IfStatementRunner.java
@@ -3,18 +3,35 @@ package com.in28minutes.ifstatement.examples;
 public class IfStatementRunner {
 
 	public static void main(String[] args) {
-		int i = 24;
+		puzzle1();
 
-		// i is 25
-		// i is 24
-		// i is neither 25 or 24
+//		int i = 24;
+//
+//		// i is 25
+//		// i is 24
+//		// i is neither 25 or 24
+//
+//		if (i == 25) {
+//			System.out.println("i = 25");
+//		} else if (i == 24) {
+//			System.out.println("i = 24");
+//		} else {
+//			System.out.println("i != 25 and i != 24");
+//		}
 
-		if (i == 25) {
-			System.out.println("i = 25");
-		} else if (i == 24) {
-			System.out.println("i = 24");
+	}
+
+	private static void puzzle1() {
+		int k = 15;
+
+		if (k > 20) {
+			System.out.println(1);
+		} else if (k > 10) {
+			System.out.println(2);
+		} else if (k < 20) {
+			System.out.println(3);
 		} else {
-			System.out.println("i != 25 and i != 24");
+			System.out.println(4);
 		}
 	}
 
