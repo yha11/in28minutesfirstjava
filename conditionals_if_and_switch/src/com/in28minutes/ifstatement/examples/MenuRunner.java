021f15b2274757b2262f38c78cfa95d096d51fbc
user <user@20.20.5.156>
2024-01-22 13:45:08 +0900

    연산

---
+++ conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/MenuRunner.java
@@ -0,0 +1,44 @@
+package com.in28minutes.ifstatement.examples;
+
+import java.util.Scanner;
+
+public class MenuRunner {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter Number1: ");
+		int number1 = scanner.nextInt();
+
+		System.out.print("Enter Number2: ");
+		int number2 = scanner.nextInt();
+
+		System.out.println("Choices Available are");
+		System.out.println("1 - Add");
+		System.out.println("2 - Subtract");
+		System.out.println("3 - Divide");
+		System.out.println("4 - Multiply");
+
+		System.out.print("Enter Choice: ");
+		int choice = scanner.nextInt();
+
+		System.out.println("Your choices are");
+		System.out.println("Number1 " + number1);
+		System.out.println("Number2 " + number2);
+		System.out.println("Choice " + choice);
+
+		if (choice == 1) {
+			System.out.println(number1 + number2);
+		} else if (choice == 2) {
+			System.out.println(number1 - number2);
+		} else if (choice == 3) {
+			System.out.println(number1 / number2);
+		} else if (choice == 4) {
+			System.out.println(number1 * number2);
+		} else {
+			System.out.println("Invalid Operation");
+		}
+
+	}
+
+}
