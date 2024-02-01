e66b93911f62c858e6a627e59faf763319bf5171
user <user@20.20.5.199>
2024-02-01 15:40:35 +0900

    case 문

--- conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/SwitchExercisesRunner.java
+++ conditionals_if_and_switch/src/com/in28minutes/ifstatement/examples/SwitchExercisesRunner.java
@@ -3,37 +3,75 @@ package com.in28minutes.ifstatement.examples;
 public class SwitchExercisesRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(isWeekDay(6));
 	}
 
 	public static boolean isWeekDay(int dayNumber) {
 		switch (dayNumber) {
-		case 0:
-			System.out.println("Sunday");
-			return false;
+		// case 0:
+		// case 6:
+		// return false;
 		case 1:
-			System.out.println("Monday");
-			return true;
 		case 2:
-			System.out.println("Tuesday");
-			return true;
 		case 3:
-			System.out.println("Wednesday");
-			return true;
 		case 4:
-			System.out.println("Thursday");
-			return true;
 		case 5:
-			System.out.println("Friday");
 			return true;
-		case 6:
-			System.out.println("Saturday");
-			return false;
-		default:
-			System.out.println("잘못 입력하셨습니다.");
+
 		}
 		return false;
+	}
+
+	public static String determineNameOfMonth(int monthNumber) {
+		switch (monthNumber) {
+		case 1:
+			return "January";
+		case 2:
+			return "February";
+		case 3:
+			return "March";
+		case 4:
+			return "April";
+		case 5:
+			return "May";
+		case 6:
+			return "June";
+		case 7:
+			return "July";
+		case 8:
+			return "August";
+		case 9:
+			return "September";
+		case 10:
+			return "October";
+		case 11:
+			return "November";
+		case 12:
+			return "December";
+		default:
+			return "잘못 입력하셨습니다.";
+		}
+	}
+
+	public static String determineNameOfDay(int dayNumber) {
+		switch (dayNumber) {
+		case 0:
+			return "Sunday";
+		case 1:
+			return "Monday";
+		case 2:
+			return "Tuesday";
+		case 3:
+			return "Wednesday";
+		case 4:
+			return "Thursday";
+		case 5:
+			return "Friday";
+		case 6:
+			return "Saturday";
+		default:
+			return "잘못 입력하셨습니다.";
+		}
 	}
 
 }
